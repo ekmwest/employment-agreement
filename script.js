d83531ea91a095a5169664f62d66880c1723e736
Karl West <mickel.west@gmail.com>
2023-03-25 01:08:45 +0100

    refactor

--- script.js
+++ script.js
@@ -2,13 +2,9 @@
    Constants
    ========================================================================== */
 
-const STORAGE_KEY = 'data';
+const STORAGE_KEY = 'agreement';
 
-const PARAM = {
-    AGREEMENT_ID: 'id'
-}
-
-const MODEL_PROPERTY = {
+const MODEL = {
     EMPLOYEE_NAME: 'EMPLOYEE_NAME',
     EMPLOYEE_SOCIAL_SECURITY_NUMBER: 'EMPLOYEE_SOCIAL_SECURITY_NUMBER',
     EMPLOYEE_ADDRESS: 'EMPLOYEE_ADDRESS',
@@ -21,196 +17,19 @@ const MODEL_PROPERTY = {
     SALARY_ACCOUNT_NUMBER: 'SALARY_ACCOUNT_NUMBER'
 }
 
-const SELECTOR = Object.fromEntries(Object.entries(MODEL_PROPERTY).map(([k, v]) => [k, `.${pascalToHyphens(v)}`]));
+const SELECTOR = Object.fromEntries(Object.entries(MODEL).map(([k, v]) => [k, `.${pascalToHyphens(v)}`]));
 
 
 
 /* ==========================================================================
-   Load Handler
+   Load Handlers
    ========================================================================== */
 
-document.addEventListener('DOMContentLoaded', loadAgreement);
-
-
-
-/* ==========================================================================
-   Load Agreement
-   ========================================================================== */
-
-function loadAgreement() {
-    const agreementId = getAgreementId();
-    const [agreementData, agreementTemplateData] = getAgreementData(agreementId);
-    hydrate(agreementData, agreementTemplateData);
-}
-
-
-
-/* ==========================================================================
-   Get Agreement Data
-   ========================================================================== */
-
-function getAgreementData(agreementId) {
-    if (!agreementId) {
-        return [getAgreementTemplateData(), getAgreementTemplateData()];
-    }
-
-    return [getAgreementDataFromStorage(agreementId), getAgreementTemplateData()];
-}
-
-
-
-/* ==========================================================================
-   Get Agreement Id
-   ========================================================================== */
-
-function getAgreementId() {
-    const urlObject = new URL(location);
-    return (new URLSearchParams(urlObject.search)).get(PARAM.AGREEMENT_ID);
-}
-
-
-
-/* ==========================================================================
-   Get Agreement Template Data
-   ========================================================================== */
-
-function getAgreementTemplateData() {
-    return {
-        [MODEL_PROPERTY.EMPLOYEE_NAME]: 'Anna Bengtsson',
-        [MODEL_PROPERTY.EMPLOYEE_SOCIAL_SECURITY_NUMBER]: '990102-1234',
-        [MODEL_PROPERTY.EMPLOYEE_ADDRESS]: 'Cykelstigen 1',
-        [MODEL_PROPERTY.EMPLOYEE_POSTAL_NUMBER]: '123 45',
-        [MODEL_PROPERTY.EMPLOYEE_CITY]: 'Diplomatstaden',
-        [MODEL_PROPERTY.EMPLOYMENT_BEGINS]: '1 jan 2024',
-        [MODEL_PROPERTY.EMPLOYMENT_ROLE]: 'Designer',
-        [MODEL_PROPERTY.SALARY_INITIAL_AMOUNT]: '23 400',
-        [MODEL_PROPERTY.SALARY_ACCOUNT_BANK_NAME]: 'Nordea',
-        [MODEL_PROPERTY.SALARY_ACCOUNT_NUMBER]: '1234-1, 3232 765-0',
-    };
-}
-
-
-
-/* ==========================================================================
-   Crate New Agreement
-   ========================================================================== */
-
-function createNewAgreement() {
-    const agreementId = crypto.randomUUID();
-    location.assign(`/edit.html?id=${agreementId}`);
-}
-
-
-
-/* ==========================================================================
-   Save Form
-   ========================================================================== */
-
-function saveForm() {
-
-    // TODO: VALIDATE!
-
-    const agreementId = getAgreementId();
-    const agreementData = extractAgreementDataFromForm();
-    saveAgreementToStorage(agreementId, agreementData);
-
-    location.assign(`/index.html?id=${agreementId}`);
-}
-
-
-
-/* ==========================================================================
-   Extract Agreement Data From Form
-   ========================================================================== */
-
-function extractAgreementDataFromForm() {
-    const data = {};
-    for (const [key, value] of Object.entries(SELECTOR)) {
-        const input = document.querySelector(value);
-
-        if (!input) {
-            console.log('Could not find input', value);
-            continue;
-        }
-
-        data[key] = input.value;
-    }
-
-    return data;
-}
-
-
-
-/* ==========================================================================
-   Get Agreement Data From Storage
-   ========================================================================== */
-
-function saveAgreementToStorage(agreementId, agreementData) {
-    const db = DB.get();
-    db[agreementId] = agreementData;
-    DB.set(db);
-}
-
-
-
-/* ==========================================================================
-   Get Agreement Data From Storage
-   ========================================================================== */
-
-function getAgreementDataFromStorage(agreementId) {
-    const db = DB.get();
-    const agreement = db[agreementId];
-    return agreement || {};
-}
-
-
-
-/* ==========================================================================
-   Hydrate
-   ========================================================================== */
-
-function hydrate(employmentAgreementData, employmentAgreementTemplateData) {
-
-    // If employmentAgreementData is empty, loop will not run
-    // This happens when edit new agreement. Only template data
-    // is avaiable
-
-    for (const [key, value] of Object.entries(employmentAgreementData)) {
-
-        const element = document.querySelector(SELECTOR[key]);
-        if (!element) {
-            console.log('Could not hydrate element (not found):', key);
-            continue;
-        }
-
-        if (element.tagName === 'INPUT') {
-            element.value = value;
-            element.setAttribute('value', value);
-            element.placeholder = employmentAgreementTemplateData[key];
-        } else {
-            element.innerText = value;
-        }
-
-    }
-}
-
-
-
-/* ==========================================================================
-   Pascal To Hyphens
-   ========================================================================== */
-
-function pascalToHyphens(pascal) {
-    // SimpleConfirm => simple-confirm
-    // SIMPLE_CONFIRM => simple-confirm
-    return pascal.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase().replaceAll('_', '-');
-};
-
-
-
-/* ==========================================================================
-   Print Modifier
-   ========================================================================== */
+document.addEventListener('DOMContentLoaded', () => {
+    const agreement = getAgreement();
+    const defaultAgreement = getDefaultAgreement();
+    hydrate(agreement, defaultAgreement);
+});
 
 window.addEventListener('beforeprint', event => {
     document.documentElement.classList.add('print');
@@ -223,10 +42,120 @@ window.addEventListener('afterprint', event => {
 
 
 /* ==========================================================================
-   DB
+   Agreement
    ========================================================================== */
 
-const DB = {
+function getAgreement() {
+    if (isEmpty(getStoredAgreement())) {
+        return getDefaultAgreement()
+    }
+    return getStoredAgreement();
+}
+
+function getStoredAgreement() {
+    return Store.get();
+}
+
+function getDefaultAgreement() {
+    return {
+        [MODEL.EMPLOYEE_NAME]: 'Anna Bengtsson',
+        [MODEL.EMPLOYEE_SOCIAL_SECURITY_NUMBER]: '990102-1234',
+        [MODEL.EMPLOYEE_ADDRESS]: 'Cykelstigen 1',
+        [MODEL.EMPLOYEE_POSTAL_NUMBER]: '123 45',
+        [MODEL.EMPLOYEE_CITY]: 'Diplomatstaden',
+        [MODEL.EMPLOYMENT_BEGINS]: '1 jan 2024',
+        [MODEL.EMPLOYMENT_ROLE]: 'Designer',
+        [MODEL.SALARY_INITIAL_AMOUNT]: '23 400',
+        [MODEL.SALARY_ACCOUNT_BANK_NAME]: 'Nordea',
+        [MODEL.SALARY_ACCOUNT_NUMBER]: '1234-1, 3232 765-0',
+    };
+}
+
+
+
+/* ==========================================================================
+   Form Functions
+   ========================================================================== */
+
+function saveForm() {
+    const agreement = extractAgreementFromForm();
+    Store.set(agreement);
+    location.assign('/');
+}
+
+function resetForm() {
+    Store.set(null);
+    location.assign('/edit.html');
+}
+
+function extractAgreementFromForm() {
+
+    // TODO: Validate
+
+    const agreement = {};
+
+    for (const [key, value] of Object.entries(SELECTOR)) {
+        const input = document.querySelector(value);
+
+        if (!input) {
+            console.log('Could not find input', value);
+            continue;
+        }
+
+        agreement[key] = input.value;
+    }
+
+    return agreement;
+}
+
+
+
+/* ==========================================================================
+   Hydrate
+   ========================================================================== */
+
+function hydrate(agreement, defaultAgreement) {
+
+    for (const [key, value] of Object.entries(agreement)) {
+
+        const element = document.querySelector(SELECTOR[key]);
+
+        if (!element) {
+            console.log('Could not hydrate element (not found):', key);
+            continue;
+        }
+
+        if (element.tagName === 'INPUT') {
+            element.value = value;
+            element.setAttribute('value', value);
+            element.setAttribute('placeholder', defaultAgreement[key]);
+        } else {
+            element.innerText = value;
+        }
+    }
+}
+
+
+
+/* ==========================================================================
+   Helpers
+   ========================================================================== */
+
+function pascalToHyphens(pascal) {
+    return pascal.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase().replaceAll('_', '-');
+};
+
+function isEmpty(obj) {
+    return Object.keys(obj).length === 0;
+}
+
+
+
+/* ==========================================================================
+   Store
+   ========================================================================== */
+
+const Store = {
     get: () => JSON.parse(localStorage.getItem(STORAGE_KEY)) || {},
-    set: data => localStorage.setItem(STORAGE_KEY, JSON.stringify(data || {}))
+    set: agreement => localStorage.setItem(STORAGE_KEY, JSON.stringify(agreement || {}))
 }
